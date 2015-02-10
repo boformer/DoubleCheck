eb950136d612280cf620d9e2ce97a0b426b67202
boformer <felix.free@gmx.de>
2015-02-10 08:25:31 +0100

    added javadoc project overview and package-info

---
+++ src/main/java/com/github/boformer/doublecheck/api/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * The relevant package for plugin developers: Contains the confirmation request API
+ */
+package com.github.boformer.doublecheck.api;
---
+++ src/main/java/com/github/boformer/doublecheck/impl/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Contains the implementation of the {@code ConfirmationService} and the plugin classes
+ */
+package com.github.boformer.doublecheck.impl;
