d363799d85e1630ba1489c42a30434b2d0acd8d3
boformer <felix.free@gmx.de>
2015-03-31 16:25:51 +0200

    format

--- src/main/java/com/github/boformer/doublecheck/DoubleCheck.java
+++ src/main/java/com/github/boformer/doublecheck/DoubleCheck.java
@@ -21,48 +21,52 @@ public class DoubleCheck
 {
 	/**
 	 * The default message that is displayed after when a request is sent:
-	 * 
+	 *
 	 * <p>{@code "You have 15 seconds to /confirm or /deny the action."}</p>
 	 */
 	//TODO translations
-	public final static Text REQUEST_MESSAGE = Texts.of(TextColors.GOLD, "You have 15 seconds to ", TextColors.RED, "/confirm", 
-			TextColors.GOLD, " or ", TextColors.RED, "/deny", TextColors.GOLD, " the action.");
-	
+	public final static Text REQUEST_MESSAGE = Texts.of(TextColors.GOLD, "You have 15 seconds to ", TextColors.RED, "/confirm", TextColors.GOLD,
+			" or ", TextColors.RED, "/deny", TextColors.GOLD, " the action.");
+
 	/**
 	 * The default alias of the confirm command ({@code "confirm"}).
 	 */
 	public final static String CONFIRM_ALIAS = "confirm";
-	
+
 	/**
 	 * The default alias of the deny command ({@code "deny"}).
 	 */
 	public final static String DENY_ALIAS = "deny";
-	
+
 	/**
 	 * The default expiration time of a request (15 seconds).
 	 */
 	public final static int EXPIRATION_TIME = 15;
-	
+
+
 	/**
-	 * Initializes a new {@link ConfirmationService} with the default parameters.
-	 * 
+	 * Initializes a new {@link ConfirmationService} with the default
+	 * parameters.
+	 *
 	 * <p>This is the recommended way to create the service.</p>
-	 * 
+	 *
 	 * @param game The game
 	 * @param plugin The plugin instance
 	 * @return The service
 	 */
-	public static ConfirmationService initializeService(Game game, Object plugin) 
+	public static ConfirmationService initializeService(Game game, Object plugin)
 	{
 		return initializeService(game, plugin, REQUEST_MESSAGE, CONFIRM_ALIAS, DENY_ALIAS, EXPIRATION_TIME);
 	}
-	
+
 	/**
-	 * Initializes a new {@link ConfirmationService} with the specified parameters.
-	 * 
+	 * Initializes a new {@link ConfirmationService} with the specified
+	 * parameters.
+	 *
 	 * <p>Only use this if the default message and aliases are not suitable.
-	 * Otherwise, use the recommended {@link DoubleCheck#initializeService(Game, Object)} method.</p>
-	 * 
+	 * Otherwise, use the recommended
+	 * {@link DoubleCheck#initializeService(Game, Object)} method.</p>
+	 *
 	 * @param game The game
 	 * @param plugin The plugin instance
 	 * @param requestMessage The request message
@@ -71,48 +75,47 @@ public class DoubleCheck
 	 * @param expirationTime The expiration time in seconds
 	 * @return The service
 	 */
-	public static ConfirmationService initializeService(Game game, Object plugin, Text requestMessage, String confirmAlias, 
-			String denyAlias, int expirationTime) 
+	public static ConfirmationService initializeService(Game game, Object plugin, Text requestMessage, String confirmAlias, String denyAlias,
+			int expirationTime)
 	{
 		checkNotNull(game);
 		checkNotNull(plugin);
-		
+
 		ConfirmationService service = new DoubleCheckService(game, requestMessage, confirmAlias, denyAlias, expirationTime);
 		game.getEventManager().register(plugin, service);
-		
+
 		return service;
 	}
-	
-	private static class DoubleCheckService implements ConfirmationService 
+
+
+	private static class DoubleCheckService implements ConfirmationService
 	{
 		private final Cache<CommandSource, Request> requestCache;
-		
+
 		private final Game game;
-		
+
 		private final Text requestMessage;
 		private final String confirmAlias;
 		private final String denyAlias;
-		
+
+
 		public DoubleCheckService(Game game, Text requestMessage, String confirmAlias, String denyAlias, int expirationTime)
 		{
 			this.game = game;
-			
-			this.requestCache = CacheBuilder.newBuilder()
-					.initialCapacity(4)
-					.expireAfterWrite(expirationTime, TimeUnit.SECONDS)
-					.build();
-			
+
+			requestCache = CacheBuilder.newBuilder().initialCapacity(4).expireAfterWrite(expirationTime, TimeUnit.SECONDS).build();
+
 			this.requestMessage = requestMessage;
 			this.confirmAlias = confirmAlias;
 			this.denyAlias = denyAlias;
 		}
-		
+
 		@Override
 		public void send(CommandSource recipient, Request request)
 		{
-			this.requestCache.put(recipient, request);
-			recipient.sendMessage(request.getMessage(), this.requestMessage);
-			
+			requestCache.put(recipient, request);
+			recipient.sendMessage(request.getMessage(), requestMessage);
+
 			// Occupy the commands
 			game.getEventManager().post(new CommandOccupationEvent(recipient, confirmAlias));
 			game.getEventManager().post(new CommandOccupationEvent(recipient, denyAlias));
@@ -129,51 +132,51 @@ public class DoubleCheck
 		{
 			requestCache.invalidate(recipient);
 		}
-		
+
 		@Subscribe
-		public void onCommand(CommandEvent event) 
+		public void onCommand(CommandEvent event)
 		{
 			boolean confirm = event.getCommand().equalsIgnoreCase(confirmAlias);
 			boolean deny = event.getCommand().equalsIgnoreCase(denyAlias);
-			
-			if(confirm || deny) 
+
+			if (confirm || deny)
 			{
 				Request request = requestCache.getIfPresent(event.getSource());
-				if(request != null) 
+				if (request != null)
 				{
 					event.setCancelled(true);
-					if(confirm)	request.confirm(event.getSource());
+					if (confirm) request.confirm(event.getSource());
 					else request.deny(event.getSource());
 				}
 			}
 		}
-		
+
 		@Subscribe
-		public void onCommandOccupation(CommandOccupationEvent event) 
+		public void onCommandOccupation(CommandOccupationEvent event)
 		{
-			if(event.getCommand().equalsIgnoreCase(confirmAlias) || event.getCommand().equalsIgnoreCase(denyAlias)) 
-			{
+			if (event.getCommand().equalsIgnoreCase(confirmAlias) || event.getCommand().equalsIgnoreCase(denyAlias)) 
 				requestCache.invalidate(event.getSource());
-			}
 		}
-		
+
+
 		private class CommandOccupationEvent extends AbstractEvent
 		{
 			private final CommandSource source;
 			private final String command;
-			
+
+
 			public CommandOccupationEvent(CommandSource source, String command)
 			{
 				super();
 				this.source = source;
 				this.command = command;
 			}
-			
+
 			CommandSource getSource()
 			{
 				return source;
 			}
-			
+
 			String getCommand()
 			{
 				return command;
